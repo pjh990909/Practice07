fe0b03826bee4e705f46de9815c49f7009d28227
pjh990909 <pjh990909@naver.com>
2024-01-10 18:11:54 +0900

    과제

--- src/com/javaex/ex02/Bird.java
+++ src/com/javaex/ex02/Bird.java
@@ -1,8 +1,33 @@
 package com.javaex.ex02;
 
 public abstract class Bird {
-    private String name;
 
+	private String name;
 
+	public Bird() {
+	}
+
+	public Bird(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Bird [name=" + name + "]";
+	}
+
+	public abstract void showName();
+
+	public abstract void sing();
+
+	public abstract void fly();
 
 }
--- src/com/javaex/ex02/Sparrow.java
+++ src/com/javaex/ex02/Sparrow.java
@@ -2,6 +2,21 @@ package com.javaex.ex02;
 
 public class Sparrow extends Bird {
 
+	
+	
+	
+	
+	public void sing() {
+		System.out.println("오리("+ getName() + ")가 소리내어 웁니다");
 
+	}
+
+	public void fly() {
+
+	}
+
+	public void showName() {
+
+	}
 
 }
